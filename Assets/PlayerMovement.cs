82230d343f52a07be7defa6ce9c4e12b22414593
maubertyretarino <mpfyretarino@gmail.com>
2020-10-25 01:07:53 +0800

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour {
+
+    //References
+    public Rigidbody Rb;
+    //automatic force; used to make the car run automatically
+    public float forwardForce = 2000f;
+    public float sideForce = 100;
+    // Update is called once per frame
+    void FixedUpdate()
+   
+    {
+        //Player Movement
+        Rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            Rb.AddForce(100 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            Rb.AddForce(-100 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/WaypointController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointController : MonoBehaviour {
+
+    //References
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f; //If the distance between the enemy and the Waypoint is less than this, then it has reacehd the waypoint
+    private int lastWaypointIndex;
+
+    private float movementSpeed = 20.0f;
+
+    // Starts after first frame update
+    void Start() {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+    // Updates once per frame
+    void Update() {
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWaypoint(distance);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+
+    }
+
+    //Checks the distance if the wall is within the distance of the Waypoint.
+    void CheckDistanceToWaypoint(float currentDistance)
+    {
+        if (currentDistance <= minDistance)
+        {
+            targetWaypointIndex++;
+            UpdateTargetWaypoint();
+        }
+    }
+    void UpdateTargetWaypoint()
+    {
+        // resets the targetWaypointIndex to the first Waypoint in the list which creates a loot for the wall.
+        if (targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+
+}
+
